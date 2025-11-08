268662823b58964fcbcb2a89a4a2ef9894884a21
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-11-08 21:25:42 +0530

    db set

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -67,9 +67,9 @@ ASGI_APPLICATION = 'core.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'chatdb'),
-        'USER': os.getenv('DB_USER', 'user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'pass'),
+        'NAME': os.getenv('DB_NAME', 'chat_app'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'manshu'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from chat import views as chat_views
+from django.http import JsonResponse
+
 
 router = routers.DefaultRouter()
 router.register(r'conversations', chat_views.ConversationViewSet, basename='conversation')
 router.register(r'messages', chat_views.MessageViewSet, basename='message')
 
+def home(request):
+    return JsonResponse({'message': 'AI Chat API is running 🚀'})
+
 urlpatterns = [
+     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/ai/', include('ai_module.urls')),
